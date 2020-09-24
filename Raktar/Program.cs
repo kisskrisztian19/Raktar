4456969154c9d383560df3cf8a1d415706ee7e21
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-09-24 08:30:27 +0200

    20200924 Raktár

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -9,9 +9,28 @@ namespace Raktar
 {
     class Program
     {
+        static List<Termek> termekek = new List<Termek>();
+        static void BeolvasRaktar()
+        {
+            StreamReader raktar = new StreamReader("raktar.csv");
+            while (!raktar.EndOfStream)
+            {
+                /*
+                 * sor[0]-> Kód
+                 *  sor[1] -> Név
+                 * sor[2] -> Ár
+                 * sor[3] -> Darabszám
+                 */
+                 string[] sor = raktar.ReadLine().Split(';');
+                
+                Termek t = new Termek(sor[0], sor[1], int.Parse(sor[2]), int.Parse(sor[3]));
+                termekek.Add(t);
+            }
+            raktar.Close();
+        }
         static void Main(string[] args)
         {
-
+            BeolvasRaktar();
             Console.ReadKey();
         }
     }
